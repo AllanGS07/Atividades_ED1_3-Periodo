1f15f1dd7139d9570e83d2a28e30404d4ded4ee9
AllanGS <118400833+AllanGS07@users.noreply.github.com>
2025-06-11 20:39:29 -0300

    Implementação do sistema de controle de pista de decolagem com adição de aviões, autorização de decolagem e listagem. Criação da classe Aviao para representar os aviões. Implementação da inversão de fila de números e remoção de caracteres de uma string usando pilhas. Adição de funcionalidade para verificar se uma sequência é palíndroma.

---
+++ Aula 11/filanumeros4/InverterFila.java
@@ -0,0 +1,39 @@
+package filanumeros4;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
+
+public class InverterFila {
+
+    public static Queue<Integer> inverter(Queue<Integer> f1) {
+        Deque<Integer> pilhaAuxiliar = new ArrayDeque<>();
+        Queue<Integer> f2 = new LinkedList<>();
+
+        while (!f1.isEmpty()) {
+            pilhaAuxiliar.push(f1.poll());
+        }
+
+        while (!pilhaAuxiliar.isEmpty()) {
+            f2.add(pilhaAuxiliar.pop());
+        }
+
+        return f2;
+    }
+
+    public static void main(String[] args) {
+        Queue<Integer> filaF1 = new LinkedList<>();
+        Random random = new Random();
+
+        for (int i = 0; i < 100; i++) {
+            filaF1.add(random.nextInt(1000));
+        }
+
+        System.out.println("Fila Original (F1): " + filaF1);
+        Queue<Integer> filaF2 = inverter(filaF1);
+
+        System.out.println("\nFila Invertida (F2): " + filaF2);
+    }
+}

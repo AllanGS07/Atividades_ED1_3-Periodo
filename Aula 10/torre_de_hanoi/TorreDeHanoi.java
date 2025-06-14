a001987d46fdc04b74340611233c9d3c4b174552
AllanGS <118400833+AllanGS07@users.noreply.github.com>
2025-06-13 21:48:27 -0300

    Refatoração do método resolverHanoi para retornar a quantidade de movimentos e remoção de prints de movimentação. Ajuste na exibição do tempo de execução e quantidade de movimentos no método main.

--- Aula 10/torre_de_hanoi/TorreDeHanoi.java
+++ Aula 10/torre_de_hanoi/TorreDeHanoi.java
@@ -3,14 +3,16 @@ package torre_de_hanoi;
 import java.util.Scanner;
 
 public class TorreDeHanoi {
-    public static void resolverHanoi(int numeroDiscos, char torreOrigem, char torreDestino, char torreAuxiliar) {
+    public static long resolverHanoi(int numeroDiscos, char torreOrigem, char torreDestino, char torreAuxiliar) {
+        long movimentos = 0;
         if (numeroDiscos == 1) {
-            System.out.println("Mover disco 1 da torre " + torreOrigem + " para a torre " + torreDestino);
-            return;
+            movimentos = 1;
+        } else {
+            movimentos += resolverHanoi(numeroDiscos - 1, torreOrigem, torreAuxiliar, torreDestino);
+            movimentos++;
+            movimentos += resolverHanoi(numeroDiscos - 1, torreAuxiliar, torreDestino, torreOrigem);
         }
-        resolverHanoi(numeroDiscos - 1, torreOrigem, torreAuxiliar, torreDestino);
-        System.out.println("Mover disco " + numeroDiscos + " da torre " + torreOrigem + " para a torre " + torreDestino);
-        resolverHanoi(numeroDiscos - 1, torreAuxiliar, torreDestino, torreOrigem);
+        return movimentos;
     }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -19,10 +21,8 @@ public class TorreDeHanoi {
         scanner.close();
 
         long inicio = System.currentTimeMillis();
-        System.out.println("\nResolvendo Torres de Hanói para " + numeroDeDiscos + " discos:");
-        resolverHanoi(numeroDeDiscos, 'A', 'C', 'B');
+        long totalMovimentos = resolverHanoi(numeroDeDiscos, 'A', 'C', 'B');
 
-        // Cálculo do tempo de execução
         long fim = System.currentTimeMillis();
         long total = fim - inicio;
         long horas = total / 3600000;
@@ -31,7 +31,9 @@ public class TorreDeHanoi {
         resto %= 60000;
         long segundos = resto / 1000;
         long milissegundos = resto % 1000;
-        System.out.printf("Tempo de execução: %02d:%02d:%02d:%03d%n", horas, minutos, segundos, milissegundos);
 
+        System.out.println("Quantidade de movimentos: " + totalMovimentos);
+        System.out.printf("Tempo de execução: %02d:%02d:%02d:%03d%n", horas, minutos, segundos, milissegundos);
+        scanner.close();
     }
 }
